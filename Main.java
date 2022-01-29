7cbd29b6543ea56fa35c398a6ef7fe2776565a7f
unknown <adityaraj715426@gmail.com>
2022-01-29 16:23:40 +0530

    added the LIS and upper_bound method

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class Main {
 	// initialize variable
-
 	// Initialize the reader
 	private static FastScanner scan = new FastScanner();
 	// Initialize the writer
@@ -13,22 +12,70 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		/**********************************************************************************************************************************/
-		int t = scan.readInt();
+		int t = 1;
+		t = scan.readInt();
+
 		while (t != 0) {
 			solve();
 			t--;
 		}
+
 		/**************************************************************************************************************************************/
 	}
 
 	/**************************************************************************************************************************************/
 	// Function
-	public static void solve() throws IOException{
-		// writer your code here hello 
-
+	public static void solve() throws IOException {
+		int[] arr = scan.readIntArray(2);
+		int n = arr[0];
+		int m = arr[1];
+		int[] a = scan.readIntArray(n);
+		int[] b = scan.readIntArray(m);
+		out.writeInt(LIS(a) + LIS(b));
 	}
 
 	/**************************************************************************************************************************************/
+	static int LIS(int[] arr){
+		int n = arr.length;
+		ArrayList<Integer> lis = new ArrayList<>();
+		lis.add(arr[0]);
+		for (int i = 1; i < n; i++) {
+			if (lis.get(lis.size() - 1) <=arr[i]) {
+				lis.add(arr[i]);
+			} else {
+				int idx = upper_bound(lis, 0, lis.size(), arr[i]);
+				lis.set(idx, arr[i]);
+			}
+		}
+		return lis.size();
+	}
+
+	public static int upper_bound(ArrayList<Integer> lis, int low, int high, int k) {
+		int s = low;
+		int e = high;
+		while (s != e) {
+			int mid = s + e >> 1;
+			if (lis.get(mid) <= k) {
+				s = mid + 1;
+			} else {
+				e = mid;
+			}
+		}
+		if (s == lis.size()) {
+			return -1;
+		}
+		return s;
+	}
+
+	public static int nextpoweroftwo(long n) {
+		int i = 0;
+		while (n != 0) {
+			n >>= 1;
+			i++;
+		}
+		return i;
+	}
+
 	public static long gcd(long a, long b) {
 		if (b == 0)
 			return a;
@@ -55,20 +102,21 @@ public class Main {
 		}
 		return true;
 	}
-	public static List<Integer> primefactorization(int n){
+
+	public static List<Integer> primefactorization(int n) {
 		List<Integer> list = new ArrayList<>();
-		for(int i = 2; i<=Math.sqrt(n);i++){
-			while(n%i==0){
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			while (n % i == 0) {
 				list.add(i);
-				n/=i;
+				n /= i;
 			}
 		}
-
-		if(n!=1) list.add(n) ;
+		if (n != 1)
+			list.add(n);
 		return list;
 	}
 
-	public static int countbit(Long n) {
+	public static int countbit(long n) {
 		int count = 0;
 		while (n > 0) {
 			count++;
@@ -85,238 +133,229 @@ public class Main {
 		return false;
 	}
 
-	/*************************************************************************************************************************/
-	/*************************************************************************************************************************/
-	// pair class of Pair<Integer, Integer>
-	// like list,queue,etc;(Integer,Integer)
-	// Pair class of Generic type
+	public static boolean poweroftwo(long n) {
+		if ((n & n - 1) == 0)
+			return true;
+		return false;
+	}
 
-	static class Pair<A, B> {
-		A first;
-		B second;
+	public static List<Integer> ConvertToList(int[] arr) {
+		List<Integer> lis = new ArrayList<>();
+		for (int i = 0; i < arr.length; i++)
+			lis.add(arr[i]);
+		return lis;
+	}
+}
 
-		Pair(A first, B second) {
-			this.first = first;
-			this.second = second;
+/*************************************************************************************************************************/
+/*************************************************************************************************************************/
+// pair class of Pair<Integer, Integer>
+// like list,queue,etc;(Integer,Integer)
+// Pair class of Generic type
+
+class Pair<A, B> {
+	A first;
+	B second;
+
+	Pair(A first, B second) {
+		this.first = first;
+		this.second = second;
+	}
+}
+
+/********************************************************************* */
+// Fast Reader Class
+class FastScanner {
+
+	// Reader object
+	BufferedReader reader;
+	public int[] intarr;
+
+	// Constructor
+	public FastScanner() {
+
+		// Initialize the reader
+		reader = new BufferedReader(new InputStreamReader(System.in));
+
+		if (System.getProperty("ONLINE_JUDGE") == null) {
+			try {
+				reader = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+			} catch (Exception e) {
+			}
 		}
 	}
 
-	/********************************************************************* */
-	// Fast Reader Class
-	static class FastScanner {
+	// String tokenizer
+	StringTokenizer tokenizer;
 
-		// Reader object
-		BufferedReader reader;
-		public int[] intarr;
-		public Long[] longarr;
-		public Float[] floatarr;
-		public Double[] doublearr;
+	// Function to read a
+	// single integer
+	// to extend the fast reader class writer your function here
 
-		// Constructor
-		public FastScanner() {
+	public int readInt() throws IOException {
+		return Integer.parseInt(reader.readLine());
+	}
 
-			// Initialize the reader
-			reader = new BufferedReader(new InputStreamReader(System.in));
+	// Function to read a
+	// single long
+	public long readLong() throws IOException {
+		return Long.parseLong(reader.readLine());
+	}
 
-			if (System.getProperty("ONLINE_JUDGE") == null) {
-				try {
-					reader = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
-				} catch (Exception e) {
-				}
+	// Function to read string
+	public String readString() throws IOException {
+		return reader.readLine();
+	}
+
+	// Function to read a array
+	// of numsInts integers
+	// in one line
+	public int[] readIntArray(int n) throws IOException {
+
+		intarr = new int[n];
+		tokenizer = new StringTokenizer(reader.readLine());
+		for (int i = 0; i < n; i++) {
+			intarr[i] = Integer.parseInt(tokenizer.nextToken());
+		}
+		return intarr;
+	}
+
+	public List<Integer> readIntAsList() throws IOException {
+		List<Integer> list = new ArrayList<Integer>();
+		tokenizer = new StringTokenizer(reader.readLine());
+		while (tokenizer.hasMoreTokens()) {
+			list.add(Integer.parseInt(tokenizer.nextToken()));
+		}
+		return list;
+	}
+
+	public List<Long> readLongAsList() throws IOException {
+		List<Long> list = new ArrayList<Long>();
+		tokenizer = new StringTokenizer(reader.readLine());
+		while (tokenizer.hasMoreTokens()) {
+			list.add(Long.parseLong(tokenizer.nextToken()));
+		}
+		return list;
+	}
+
+}
+
+// Fast Writer Class
+class FastOutput {
+
+	// Writer object
+	BufferedWriter writer;
+
+	// Constructor
+	public FastOutput() {
+
+		// Initialize the writer
+		writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		if (System.getProperty("ONLINE_JUDGE") == null) {
+			try {
+				writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.txt")));
+			} catch (Exception e) {
 			}
 		}
+	}
 
-		// String tokenizer
-		StringTokenizer tokenizer;
+	// Function to write the
+	// single integer
+	public void writeInt(int i) throws IOException {
+		writer.write(Integer.toString(i));
+		writer.newLine();
+		writer.flush();
+	}
 
-		// Function to read a
-		// single integer
-		// to extend the fast reader class writer your function here
+	// Function to writer a single character
+	public void writeChar(char c) throws IOException {
+		writer.write(Character.toString(c));
+		writer.newLine();
+		writer.flush();
+	}
 
-		public int readInt() throws IOException {
-			return Integer.parseInt(reader.readLine());
+	// Function to write single long
+	public void writeLong(long i) throws IOException {
+		writer.write(Long.toString(i));
+		writer.newLine();
+		writer.flush();
+	}
+
+	// Function to write String
+	public void writeString(String s) throws IOException {
+		writer.write(s);
+		writer.newLine();
+		writer.flush();
+	}
+
+	public void writeNewline() throws IOException {
+		writer.newLine();
+		writer.flush();
+	}
+
+	public void writeStringWithoutNewline(String s) throws IOException {
+		writer.write(s);
+		// writer.newLine();
+		writer.flush();
+	}
+
+	public void writeStringWithSpace(String s) throws IOException {
+		writer.write(s);
+		writer.write(" ");
+		writer.flush();
+	}
+
+	// Function to write a Integer of
+	// array with spaces in one line
+	public void writeIntArray(int[] arr) throws IOException {
+		for (int i = 0; i < arr.length; i++) {
+			writer.write(arr[i] + " ");
 		}
+		writer.newLine();
+		writer.flush();
+	}
 
-		// Function to read a
-		// single long
-		public long readLong() throws IOException {
-			return Long.parseLong(reader.readLine());
+	public void writeLongArray(Long[] arr) throws IOException {
+		for (int i = 0; i < arr.length; i++) {
+			writer.write(arr[i] + " ");
 		}
+		writer.newLine();
+		writer.flush();
+	}
 
-		// Function to read string
-		public String readString() throws IOException {
-			return reader.readLine();
+	// Function to write Integer of
+	// array without spaces in 1 line
+	public void writeIntArrayWithoutSpaces(int[] arr) throws IOException {
+		for (int i = 0; i < arr.length; i++) {
+			writer.write(Integer.toString(arr[i]));
 		}
+		writer.newLine();
+		writer.flush();
+	}
 
-		// Function to read a array
-		// of numsInts integers
-		// in one line
-		public int[] readIntArray(int n) throws IOException {
+	public void writeIntegerlist(List<Integer> list) throws IOException {
+		if (list != null) {
+			for (Integer integer : list) {
 
-			intarr = new int[n];
-			tokenizer = new StringTokenizer(reader.readLine());
-			for (int i = 0; i < n; i++) {
-				intarr[i] = Integer.parseInt(tokenizer.nextToken());
+				writer.write(integer + " ");
+
 			}
-			return intarr;
+
 		}
+		writer.newLine();
+		writer.flush();
+	}
 
-		public Float[] readfloatArray(int n) throws IOException {
-
-			floatarr = new Float[n];
-			tokenizer = new StringTokenizer(reader.readLine());
-			for (int i = 0; i < n; i++) {
-				floatarr[i] = Float.parseFloat(tokenizer.nextToken());
+	public void writeintmatrix(int[][] matrix) throws IOException {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				writer.write(matrix[i][j] + " ");
 			}
-			return floatarr;
+			writer.newLine();
 		}
 
-		public Double[] readDoubleArray(int n) throws IOException {
-
-			doublearr = new Double[n];
-			tokenizer = new StringTokenizer(reader.readLine());
-			for (int i = 0; i < n; i++) {
-				doublearr[i] = Double.parseDouble(tokenizer.nextToken());
-			}
-			return doublearr;
-		}
-
-		public Long[] readLongArray(int n) throws IOException {
-			tokenizer = new StringTokenizer(reader.readLine());
-
-			longarr = new Long[n];
-			int i = 0;
-			while (tokenizer.hasMoreTokens()) {
-				longarr[i] = Long.parseLong(tokenizer.nextToken());
-				i++;
-			}
-			return longarr;
-		}
-
-		public List<Integer> readIntAsList() throws IOException {
-			List<Integer> list = new ArrayList<Integer>();
-			tokenizer = new StringTokenizer(reader.readLine());
-			while (tokenizer.hasMoreTokens()) {
-				list.add(Integer.parseInt(tokenizer.nextToken()));
-			}
-			return list;
-		}
-
-		public List<Long> readLongAsList() throws IOException {
-			List<Long> list = new ArrayList<Long>();
-			tokenizer = new StringTokenizer(reader.readLine());
-			while (tokenizer.hasMoreTokens()) {
-				list.add(Long.parseLong(tokenizer.nextToken()));
-			}
-			return list;
-		}
+		writer.flush();
 
 	}
 
-	// Fast Writer Class
-	static class FastOutput {
-
-		// Writer object
-		BufferedWriter writer;
-
-		// Constructor
-		public FastOutput() {
-
-			// Initialize the writer
-			writer = new BufferedWriter(new OutputStreamWriter(System.out));
-			if (System.getProperty("ONLINE_JUDGE") == null) {
-				try {
-					writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.txt")));
-				} catch (Exception e) {
-				}
-			}
-		}
-
-		// Function to write the
-		// single integer
-		public void writeInt(int i) throws IOException {
-			writer.write(Integer.toString(i));
-			writer.newLine();
-			writer.flush();
-		}
-
-		// Function to writer a single character
-		public void writeChar(char c) throws IOException {
-			writer.write(Character.toString(c));
-			writer.newLine();
-			writer.flush();
-		}
-
-		// Function to write single long
-		public void writeLong(long i) throws IOException {
-			writer.write(Long.toString(i));
-			writer.newLine();
-			writer.flush();
-		}
-
-		// Function to write String
-		public void writeString(String s) throws IOException {
-			writer.write(s);
-			writer.newLine();
-			writer.flush();
-		}
-
-		public void writeStringWithoutNewline(String s) throws IOException {
-			writer.write(s);
-			// writer.newLine();
-			writer.flush();
-		}
-
-		public void writeStringWithSpace(String s) throws IOException {
-			writer.write(s);
-			writer.write(" ");
-			writer.flush();
-		}
-
-		// Function to write a Integer of
-		// array with spaces in one line
-		public void writeIntArray(int[] arr) throws IOException {
-			for (int i = 0; i < arr.length; i++) {
-				writer.write(arr[i] + " ");
-			}
-			writer.newLine();
-			writer.flush();
-		}
-
-		// Function to write Integer of
-		// array without spaces in 1 line
-		public void writeIntArrayWithoutSpaces(int[] arr) throws IOException {
-			for (int i = 0; i < arr.length; i++) {
-				writer.write(Integer.toString(arr[i]));
-			}
-			writer.newLine();
-			writer.flush();
-		}
-
-		public void writeIntegerlist(List<Integer> list) throws IOException {
-			if (list != null) {
-				for (Integer integer : list) {
-
-					writer.write(integer + " ");
-
-				}
-
-			}
-			writer.newLine();
-			writer.flush();
-		}
-
-		public void writeintmatrix(int[][] matrix) throws IOException {
-			for (int i = 0; i < matrix.length; i++) {
-				for (int j = 0; j < matrix[0].length; j++) {
-					writer.write(matrix[i][j] + " ");
-				}
-				writer.newLine();
-			}
-
-			writer.flush();
-
-		}
-
-	}
 }
